0c0970f549adabfe275d4d60de66d7f4b28917dd
Dagadev <gacub20ru@gmail.com>
2023-12-09 11:46:47 -0600

    fix auto file relay & add handleFiles

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Client } from 'discord.js-selfbot-v13'
+import { Client, type TextChannel } from 'discord.js-selfbot-v13'
 import { TOKEN } from './lib/config'
 import { CATEGORY_IDS } from './lib/data'
 
@@ -22,15 +22,48 @@ FileRelayBot.on('messageCreate', (msg) => {
 
     const categoryId = CATEGORY_IDS[parentId]
 
-    if (typeof categoryId === 'string') {
-      const guild = FileRelayBot.guilds.cache.get(GUILD_ID); const destinationChannel = guild?.channels.cache.find(c => c.parentId === categoryId && c.name === channel.name)
+    if (categoryId !== undefined) {
+      const guild = FileRelayBot.guilds.cache.get(GUILD_ID)
+      const destinationChannel = guild?.channels.cache.find(c => c.parentId === categoryId && c.name === channel.name)
+
+      const handleFiles = (channel: TextChannel) => {
+        const messages: string[] = []
+        let message = ''
+        let count = 0
+        let i = 0
+
+        msg.attachments.forEach((at) => {
+          const file = `[File ${i + 1}](${at.url})`
+          const previewMessage = (message.length > 0 ? message + ', ' : '') + file
+
+          if (count < 5 && previewMessage.length < 2000) {
+            message = previewMessage
+            count++
+            if (i + 1 === file.length) {
+              messages.push(message)
+            }
+          } else {
+            messages.push(message)
+            message = file
+            count = 0
+          }
+          i++
+        })
+
+        channel.send({ content })
+        messages.forEach(m => {
+          setTimeout(() => {
+            channel.send({ content: m })
+          }, 2000)
+        })
+      }
 
       if (destinationChannel === undefined) {
         guild?.channels.create(channel.name, { nsfw: true, parent: categoryId, type: 'GUILD_TEXT' }).then((ch) => {
-          ch.send({ content: msg.attachments.size > 0 ? content.concat(`\n\n${msg.attachments.map(at => at.url).map((url, i) => `[File ${i + 1}](${url})`).join(', ')}`) : content })
+          handleFiles(ch)
         })
       } else if (destinationChannel.type === 'GUILD_TEXT') {
-        destinationChannel.send({ content: msg.attachments.size > 0 ? content.concat(`\n\n${msg.attachments.map(at => at.url).map((url, i) => `[File ${i + 1}](${url})`).join(', ')}`) : content })
+        handleFiles(destinationChannel)
       }
     }
   }
